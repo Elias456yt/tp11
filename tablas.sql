47691bd94297a111b99e74ed2929451c6a73651e
Elias456yt <166611763+Elias456yt@users.noreply.github.com>
2024-11-13 11:13:09 -0300

    Update tablas.sql

--- tablas.sql
+++ tablas.sql
@@ -1,10 +1,10 @@
--- Crear la base de datos
+--base de datos
 CREATE DATABASE videogames_db;
 
--- Usar la base de datos creada
+
 USE videogames_db;
 
--- Crear la tabla de juegos
+
 CREATE TABLE games (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100),
@@ -12,20 +12,20 @@ CREATE TABLE games (
     genre VARCHAR(50)
 );
 
--- Crear la tabla de desarrolladores
+
 CREATE TABLE developers (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100),
     country VARCHAR(50)
 );
 
--- Crear la tabla de plataformas
+-- la tabla de plataformas
 CREATE TABLE platforms (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50)
 );
 
--- Crear la tabla de personajes
+-- personajes
 CREATE TABLE characters (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100),
